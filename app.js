ed9cae11726661f63848cd6d6d6580990c677ab4
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2023-05-03 23:36:04 +0530

    Feature: Set snake speed and render accordingly

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+let lastRenderTime = 0;
+const SNAKE_SPEED = 2;
+
 // Setup game loop.
 // This runs over and over and over again, infinitely.
 function main(currentTimestamp) {
     window.requestAnimationFrame(main);
-    console.log('Timestamp', currentTimestamp);
+
+    const secondsSinceLastRender = (currentTimestamp - lastRenderTime) / 1000;
+
+    if (secondsSinceLastRender < (1 / SNAKE_SPEED)) {
+        return;
+    }
+
+    console.log('Render');
+
+    lastRenderTime = currentTimestamp;
 }
 
 window.requestAnimationFrame(main);
